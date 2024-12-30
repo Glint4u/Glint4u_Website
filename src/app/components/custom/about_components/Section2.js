ee57df012b5641f83939bdc199e136ee64b4ab96
Jaiman Soni <118343973+JaimanSoni@users.noreply.github.com>
2024-12-30 11:02:44 +0530

    Update Section2.js

--- src/app/components/custom/about_components/Section2.js
+++ src/app/components/custom/about_components/Section2.js
@@ -9,11 +9,11 @@ export default function Section2() {
             <div className='w-full h-[1px] bg-[#ffffff21]'></div>
             <div className='flex flex-col md:flex-row justify-between py-[50px] gap-[10px]'>
                 <div className=' text-[25px] leading-[30px] md:text-[30px] md:leading-[35px] text-white md:w-[450px]'>
-                    We have build solutions for businesses that value innovation and excellence.
+                    We build solutions for businesses that value innovation and excellence.
                 </div>
                 <div className='w-full md:w-[50%] mt-[30px] md:mt-0 text-[14px] md:text-[17px] text-[#ffffffc4]'>
                     <div >
-                        "glint" symbolizes clarity, brilliance, and the spark of innovation. It represents our commitment to creating seamless, intuitive experiences that illuminate the path to success. Far from just a tool, glint is a platform that fosters collaboration, enhances productivity, and drives exceptional service delivery.
+                        "glint" symbolizes clarity, brilliance, and the spark of innovation. It represents our commitment to creating seamless, intuitive experiences that illuminate the path to success. Far from just a tool, we created a platform that fosters collaboration, enhances productivity, and drives exceptional service delivery.
                     </div>
                     <div className='mt-[20px]'>
                         The name embodies our mission to bring sharp focus and precision to every interaction, helping businesses achieve their goals with ease and efficiency.
